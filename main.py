b3123b3ead3ea59771f599b7a05229a35d0b9260
Diamond Bond <diamondbond1@gmail.com>
2021-05-31 14:19:34 +0530

    stage 1,2
    Signed-off-by: Diamond Bond <diamondbond1@gmail.com>

--- main.py
+++ main.py
@@ -15,3 +15,35 @@ for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
     overlayList.append(image)
 print(len(overlayList))
+
+header = overlayList[0]
+
+cap = cv2.VideoCapture(-1)  # linux -1, win 0/1
+cap.set(3, 1280)
+cap.set(4, 720)
+
+# high detection confidence for painting; lower if errors occur
+detector = htm.handDetector(detectionCon=0.85)
+
+while True:
+    # (1) import image
+    success, img = cap.read()
+    # flip image (mirror real life)
+    img = cv2.flip(img, 1)
+
+    # (2) find hand landmarks
+    img = detector.findHands(img)
+
+    # (3) identify upright phalanges
+    #     only draw when index finger is up
+
+    # (4) identify selection mode
+    #     only when two fingers are up
+
+    # (5) if drawing mode
+    #     only when index finger is up
+
+    # render header
+    img[0:100, 0:1280] = header
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
